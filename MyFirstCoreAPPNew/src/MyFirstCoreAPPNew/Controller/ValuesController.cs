e64b0efd14e870d6c5081c4ce14bd8df3e14c9d4
jitendra.sharma <jitendra.sharma@laitkor.com>
2018-10-24 18:29:43 +0530

    Add .Net Core Application

---
+++ MyFirstCoreAPPNew/src/MyFirstCoreAPPNew/Controller/ValuesController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyFirstCoreAPPNew.Controller
+{
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/values
+        [HttpGet]
+        [Produces("application/json")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get([FromQuery] int id, string query)
+        {
+            return Ok(new val { id = id, text = "value " + id });
+        }
+
+        // POST api/values
+        [HttpPost]
+        //public void Post([FromBody]string value)
+        public IActionResult Post([FromBody] val value)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return CreatedAtAction("Get", new { id = value.id }, value);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class val
+    {
+        public int id { get; set; }
+        [MinLength(3)]
+        public string text { get; set; }
+    }
+}
---
+++ MyFirstCoreAPPNew/src/MyFirstCoreAPPNew/Data/Todo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstCoreAPPNew.Data
+{
+    public class Todo
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+    }
+}
